cc5d9beb80a11c236ac80d74215d2e4fbebb5dbd
William Bruno <rocha_bruno@hotmail.com>
2013-04-11 12:47:17 -0300

    comportamento do zeroFill separado em outra funcao, e os devidos testes desse novo componente

--- formatWeight.js
+++ formatWeight.js
@@ -1,22 +1,27 @@
 function id(el){
     return document.getElementById(el);
 }
+function zeroFill(amount) {
+	var fill = '';
+	while(amount--) {
+		fill += '0';
+	}
+	return fill;
+}
 function formatWeight(v){
-	var integer = v.split(',')[0],
-		zeroFill = 0,
+	var integer = 0,
 		c = '';
+
+	v = v + '';
+	integer = v.split(',')[0];
 
 	v = v.replace(/\D/g, "");
 	v = v.replace(/^[0]+/, "");
 
 	if(v.length <= 4 || !integer)
 	{
-		zeroFill = 4 - v.length;
-		while(zeroFill--) {
-			c += '0';
-		}
+		c = zeroFill(4 - v.length);
 		v = '0,' + c + v;
-
 	} else {
 		v = v.replace(/^(\d{1,})(\d{4})$/, "$1,$2");
 	}
--- tests/spec/formatWeightSpec.js
+++ tests/spec/formatWeightSpec.js
@@ -1,10 +1,10 @@
 describe("formatWeight", function() {
 
 	var arrIn = [
-		'1', '12', '123', '1234', '12345', '123456', '1234567', 'abc', 'a1b2c3'
+		'1', '12', '123', '1234', '12345', '123456', '1234567'
 	],
 	arrOut = [
-		'0,0001', '0,0012', '0,0123', '0,1234', '1,2345', '12,3456', '123,4567', '0,0000', '0,0123'
+		'0,0001', '0,0012', '0,0123', '0,1234', '1,2345', '12,3456', '123,4567'
 	],
 	i,
 	arrInLength = arrIn.length;
@@ -20,5 +20,25 @@ describe("formatWeight", function() {
 
 		}(arrIn[i], arrOut[i]));
 	}
+	describe("formatWeight weird inputs", function() {
+		it( "abc" + " should be equal " + "0,0000", function() {
+			expect(formatWeight("abc")).toEqual("0,0000");
+		});
+		it( "1a2b3c" + " should be equal " + "0,0123", function() {
+			expect(formatWeight("1a2b3c")).toEqual("0,0123");
+		});
+	});
+
+	describe("zeroFill", function() {
+		it( "3" + " should be equal " + "000", function() {
+			expect(zeroFill(3)).toEqual("000");
+		});
+		it( "2" + " should be equal " + "00", function() {
+			expect(zeroFill(2)).toEqual("00");
+		});
+		it( "1" + " should be equal " + "0", function() {
+			expect(zeroFill(1)).toEqual("0");
+		});
+	});
 
 });
